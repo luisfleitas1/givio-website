60628818c712a7b89a7498884a8ef4636bd475eb
pratikr-wos <pratikr@whiteorangesoftware.com>
2025-05-21 18:02:53 +0530

    designed footer

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".feedbackSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop:true,
+    navigation: {
+        nextEl: '.prev-button',
+        prevEl: '.next-button',
+      },
+});
